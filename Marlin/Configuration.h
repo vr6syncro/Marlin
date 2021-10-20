5b55e25e334e8fb0465f47cd55182ad5c9c901cd
vr6syncro <info@vr6-syncro.de>
2021-10-20 10:22:36 +0200

    lowered acceleration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -949,7 +949,7 @@
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
 //#define DEFAULT_MAX_ACCELERATION      { 3500, 3500, 100 } //Z for Bed is not calculated and must edited for your motorized bed!
-#define DEFAULT_MAX_ACCELERATION      { 100000, 100000, 500 } //Z for Bed is not calculated and must edited for your motorized bed!
+#define DEFAULT_MAX_ACCELERATION      { 3500, 3500, 500 } //Z for Bed is not calculated and must edited for your motorized bed!
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
@@ -964,9 +964,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          20000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  10000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   30000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          1000    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   3000   // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk limits (mm/s)

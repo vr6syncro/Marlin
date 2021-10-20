6f5271b318bd97a529e8702da4971dd50148348b
vr6syncro <info@vr6-syncro.de>
2021-10-20 18:45:56 +0200

    fixed pin configuration location

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -4177,3 +4177,12 @@
  */
 //#define SOFT_RESET_VIA_SERIAL         // 'KILL' and '^X' commands will soft-reset the controller
 //#define SOFT_RESET_ON_KILL            // Use a digital button to soft-reset the controller after KILL
+
+// Laser K40 Pin description
+#define SPINDLE_DIR_PIN          -1              // disabled, unused
+#define SPINDLE_LASER_ENA_PIN    P1_00      // P1_24, logic level converter maybe necessary, active low
+#define SPINDLE_LASER_PWM_PIN    P2_00       // P2_00, to K40 PWM Pin
+#define COOLANT_FLOOD_PIN        -1         // radiator fan, 24V
+#define COOLANT_MIST_PIN         -1   // waterpump or relay, 24V
+#define AIR_EVACUATION_PIN       -1        
+#define AIR_ASSIST_PIN           HEATER_BED_PIN // attached a airassist with M8 for on and M9 for off
